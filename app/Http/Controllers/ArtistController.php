9385d67f91fe9540022e5485d3b71773531a93b2
michael J Chiumya <michaelchiumya@yahoo.co.uk>
2021-06-23 10:29:43 +0100

    initial commit

---
+++ app/Http/Controllers/ArtistController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\artist;
+use Illuminate\Http\Request;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(artist $artist)
+    {
+        //
+    }
+}
